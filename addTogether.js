751eed564e0aeaf54c3f5bb1aeef60706f628da8
stepanbacho <bachostepan@gmail.com>
2021-11-10 17:38:23 +0200

    added

---
+++ addTogether.js
@@ -0,0 +1,23 @@
+function addTogether() {
+    const [first, second] = arguments;
+    if (typeof(first) !== "number") {
+      return undefined;
+    }
+    else if (second === undefined) {
+      function addSecond(second) {
+        if (typeof(second) !== "number") {
+          return undefined;
+        }
+        else {
+          return first + second;
+        }
+      }
+      return addSecond;
+    }
+    else if (typeof(second) !== "number") {
+      return undefined;
+    }
+    else {
+      return first + second;
+    }
+  }
