5ba05f1a6b577ff9aa6b0c6286299826d6e2de45
stepanbacho <bachostepan@gmail.com>
2021-11-10 17:22:18 +0200

    rolled

---
+++ steamRoll.js
@@ -0,0 +1,11 @@
+function steamrollArray(arr) {
+    const flattenedArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        flattenedArray.push(...steamrollArray(arr[i]));
+      } else {
+        flattenedArray.push(arr[i]);
+      }
+    }
+    return flattenedArray;
+};
