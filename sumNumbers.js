83ee82a1639c9275f5c92921f128f2972299d15f
stepanbacho <bachostepan@gmail.com>
2021-11-10 10:43:31 +0200

    sum done

---
+++ sumNumbers.js
@@ -0,0 +1,14 @@
+function sumAll(arr) {
+    if(arr.length>2 || arr.length<2){
+        return 'only 2 numbers acceptable'
+    }
+    let max = Math.max(arr[0], arr[1]);
+    let min = Math.min(arr[0], arr[1]);
+    let sumBetween = 0;
+    for (let i = min; i <= max; i++) {
+        sumBetween += i;
+    }
+    return sumBetween;
+}
+let r = sumAll([2,]);
+console.log(r);
