2d925b67ae60754f67eca054b09d8fda8dacda72
stepanbacho <bachostepan@gmail.com>
2021-11-11 13:43:40 +0200

    url ++

---
+++ urlSlug.js
@@ -0,0 +1,9 @@
+var globalTitle = "Winter Is Coming";
+// Only change code below this line
+function urlSlug(title) {
+    return title
+        .toLowerCase()
+        .trim()
+        .split(/\s+/)
+        .join("-");
+}
